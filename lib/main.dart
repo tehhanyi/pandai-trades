c47fb3260b23c7a1a41b48fdbc96beec78451515
tehhanyi <hanyi.teh@beveat.com>
2024-03-19 15:08:36 +0800

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:swipe_cards/draggable_card.dart';
-import 'package:swipe_cards/swipe_cards.dart';
-
-import 'models/content_model.dart';
+import 'package:varsity_app/views/root.dart';
+import 'package:varsity_app/views/splash.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -19,161 +17,17 @@ class MyApp extends StatelessWidget {
       return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          fontFamily: 'ProximaNova',
+          brightness: Brightness.dark,
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          primaryColor: Color(0xFF1E1E2C),
           useMaterial3: true,
         ),
-        home: MyHomePage(title: ''),
+        home: SplashScreen()
+        // RootScreen(tab: 1),
       );
     });
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<SwipeItem> _swipeItems = <SwipeItem>[];
-  MatchEngine? _matchEngine;
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  List<String> _names = [
-    "Red",
-    "Blue",
-    "Green",
-    "Yellow",
-    "Orange",
-    "Grey",
-    "Purple",
-    "Pink"
-  ];
-  List<Color> _colors = [
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow,
-    Colors.orange,
-    Colors.grey,
-    Colors.purple,
-    Colors.pink
-  ];
-
-  @override
-  void initState() {
-
-    for (int i = 0; i < _names.length; i++) {
-      _swipeItems.add(SwipeItem(
-          content: Content(text: _names[i], color: _colors[i]),
-          likeAction: () {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text("Liked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          },
-          nopeAction: () {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text("Nope ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          },
-          superlikeAction: () {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text("Superliked ${_names[i]}"),
-              duration: Duration(milliseconds: 500),
-            ));
-          },
-          onSlideUpdate: (SlideRegion? region) async {
-            print("Region $region");
-          }));
-    }
-
-    _matchEngine = MatchEngine(swipeItems: _swipeItems);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        key: _scaffoldKey,
-        appBar: null,//AppBar(title: Text(widget.title!),),
-        body: Container(
-            child: Stack(
-                children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 30),
-                height: MediaQuery.of(context).size.height - kToolbarHeight,
-                child: SwipeCards(
-                  matchEngine: _matchEngine!,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      color: _swipeItems[index].content.color,
-                      child: Text(
-                        _swipeItems[index].content.text,
-                        style: TextStyle(fontSize: 100),
-                      ),
-                    );
-                  },
-                  onStackFinished: () {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("Stack Finished"),
-                      duration: Duration(milliseconds: 500),
-                    ));
-                  },
-                  itemChanged: (SwipeItem item, int index) => print("item: ${item.content.text}, index: $index"),
-                  leftSwipeAllowed: true,
-                  rightSwipeAllowed: true,
-                  upSwipeAllowed: true,
-                  fillSpace: true,
-                  likeTag: Container(
-                    margin: const EdgeInsets.all(15.0),
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.green)
-                    ),
-                    child: Text('Like'),
-                  ),
-                  nopeTag: Container(
-                    margin: const EdgeInsets.all(15.0),
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.red)
-                    ),
-                    child: Text('Nope'),
-                  ),
-                  superLikeTag: Container(
-                    margin: const EdgeInsets.all(15.0),
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.orange)
-                    ),
-                    child: Text('Super Like'),
-                  ),
-                ),
-              ),
-              // Align(
-              //   alignment: Alignment.bottomCenter,
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //     children: [
-              //       ElevatedButton(
-              //           onPressed: () {_matchEngine!.currentItem?.nope();},
-              //           child: Text("Nope")),
-              //       ElevatedButton(
-              //           onPressed: () {_matchEngine!.currentItem?.superLike();},
-              //           child: Text("Superlike")),
-              //       ElevatedButton(
-              //           onPressed: () {
-              //             _matchEngine!.currentItem?.like();
-              //           },
-              //           child: Text("Like"))
-              //     ],
-              //   ),
-              // )
-            ])));
-  }
-}
---
+++ lib/views/splash.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:varsity_app/views/root.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 3),);
+    WidgetsFlutterBinding.ensureInitialized();
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+    Future.delayed(Duration(seconds: 3)).then((value) async =>
+    await Navigator.push(context, MaterialPageRoute(builder: (context) =>  RootScreen(tab: 1)))));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // double opacityLevel = 1.0;
+  // void _changeOpacity() {
+  //   setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.forward();
+    return Container(
+        color: Theme.of(context).primaryColor,
+        child: Center(
+            child: Hero(
+              tag: "Logo",
+              //FadeTransition makes your image Fade
+              child: FadeTransition(
+                //Use your animation here
+                opacity: Tween(begin: 1.0, end: 0.0,).animate(_controller),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    new FlutterLogo(size: 100.0,),
+                    // CircleAvatar(
+                    //   //Here you load you image
+                    //   backgroundImage: AssetImage("logo.png"), //put logo here
+                    //   radius: MediaQuery.of(context).size.width * 0.25,
+                    // )
+                    Text('PandiTrades', style: TextStyle(fontSize: 30.sp, color: Colors.white,decoration: TextDecoration.none))
+                  ],
+                )
+
+              ))));
+  }
+}
