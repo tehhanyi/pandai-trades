23a2322cded08d5a36a80eebf6a1e616a89d5221
tehhanyi <hanyi.teh@beveat.com>
2024-03-24 17:40:58 +0800

    add description

--- lib/views/landing.dart
+++ lib/views/landing.dart
@@ -16,52 +16,54 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  String desc = 'Pandai Trades stands at the intersection of technology, education, and finance, offering a unique solution to the modern trades\'s dilemma. By combining a user-friendly interface with powerful technological underpinnings and educational elements, it promises to transform the way individuals engage with the stock market. Whether you\'re a seasoned trader or just starting, Pandai Trades is your gateway to mastering the art of trading, one swipe at a time.';
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  Widget About(){
+    return Column(
+      children: [
+        SizedBox(height: 5.5.h,),
+        IconButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext builderContext) {
+                    return Center(
+                        child: Column(mainAxisSize: MainAxisSize.min, children: [
+                          Container(
+                              width: 88.w,
+                              decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(Radius.circular(15))),
+                              child: Padding(
+                                padding: EdgeInsets.all(14.sp),
+                                child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Text('About PandaiTrades', style: TextStyle(fontSize: 17.sp, color: Colors.black, fontWeight: FontWeight.bold)),
+                                      SizedBox(height: 10.sp),
+                                      Text(desc, style: TextStyle(fontSize: 11.sp, color: Colors.grey)),
+                                      SizedBox(height: 10.sp),
+                                      Text('Swipe Right to add security\nSwipe Left to pass a security', style: TextStyle(fontSize: 12.sp, color: Colors.grey),textAlign: TextAlign.center,),
+                                      SizedBox(height: 10.sp),
+                                      GestureDetector(
+                                        onTap: () => Navigator.of(builderContext).pop(),
+                                        child: CircleAvatar(backgroundImage: AssetImage("assets/images/logo.png"), radius: 7.w),
+                                      )
+                                    ]),
+                              ))
+                        ]));
+                  });
+            },
+            icon:  Icon(Icons.question_mark)),
+        Spacer()
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Column(
-        children: [
-          SizedBox(height: 5.5.h,),
-          IconButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext builderContext) {
-                      return Center(
-                          child: Column(mainAxisSize: MainAxisSize.min, children: [
-                            Container(
-                                width: 88.w,
-                                decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                                child: Padding(
-                                  padding: EdgeInsets.all(14.sp),
-                                  child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Text('About PandaiTrades', style: TextStyle(fontSize: 17.sp, color: Colors.black, fontWeight: FontWeight.bold)),
-                                        SizedBox(height: 10.sp),
-                                        Text('whatever about pandaitrade is blah blah blah it is super cute and everyone should use it', style: TextStyle(fontSize: 12.sp, color: Colors.grey)),
-                                        Text('Swipe Right to add security\nSwipe Left to pass a security', style: TextStyle(fontSize: 12.sp, color: Colors.grey)),
-                                        SizedBox(height: 10.sp),
-                                        CircleAvatar(backgroundImage: AssetImage("assets/images/logo.png"), radius: 7.w),
-
-                                      ]),
-                                ))
-                          ]));
-                    });
-              },
-              icon:  Icon(Icons.question_mark)),
-              // child:,
-          // IconButton(
-          //     onPressed: (){
-          //       ;
-          //       }, icon: Icon(Icons.question_mark)),
-          Spacer()
-        ],
-      ),
+      floatingActionButton: About(),
         key: _scaffoldKey,
         appBar: AppBar(
             automaticallyImplyLeading: false,
